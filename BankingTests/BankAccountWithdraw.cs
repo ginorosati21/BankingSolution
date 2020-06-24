2fde17c1a85bca674e84d0df4bf360745167c5cc
ginorosati21 <ginorosati21@gmail.com>
2020-06-24 15:34:56 +0000

    Add project files.

---
+++ BankingTests/BankAccountWithdraw.cs
@@ -0,0 +1,36 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountWithdraw
+    {
+        [Fact]
+        public void WithdrawingMoneyDecreasesTheBalance()
+        {
+            //Given
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToWithdraw = 500M;
+
+            //When
+            account.Withdraw(amountToWithdraw);
+
+            //Then
+            var expectedBalance = openingBalance - amountToWithdraw;
+            Assert.Equal(expectedBalance, account.GetBalance());
+        }
+
+        [Fact]
+        public void YouCanTakeAllYourMoney()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            Assert.Equal(0, account.GetBalance());
+        }
+    }
+}
---
+++ BankingTests/NewAccountTests.cs
@@ -0,0 +1,27 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class NewAccountTests
+    {
+        [Fact]
+        public void NewAccountsHaveCorrectBlance()
+        {
+            // "WTCYWYH" "Write the code you wish you hd
+            //given I have a brand new bank account 
+            var account = new BankAccount();
+            //When I retrive the balance
+            decimal balance = account.GetBalance();
+            //it has a balance of $5,000
+
+            Assert.Equal(5000M, balance);
+        }
+    }
+}
+
+
+// "Test Per Type" - BankAccount -> BankAccountTests, Customer -> CustomerTests
